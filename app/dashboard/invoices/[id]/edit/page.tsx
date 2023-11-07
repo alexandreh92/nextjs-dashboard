208cd8612c76f74ee30abc3640bdc143adb8481d
Alexandre Stapenhorst <eng.alexandreh@gmail.com>
2023-11-06 21:22:23 -0300

    feat: chapter 12

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,36 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+
+interface PageParams {
+  id: string;
+}
+
+interface EditInvoicesProps {
+  params: PageParams;
+}
+
+export default async function EditInvoices({
+  params: { id },
+}: EditInvoicesProps) {
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice!} customers={customers} />
+    </main>
+  );
+}
